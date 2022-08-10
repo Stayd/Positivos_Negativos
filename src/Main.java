6990708288a9243138a8db4518792b32ebe920b6
Stayd <sebas1camacho@gmail.com>
2022-08-10 13:05:40 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        int numerolf = 1;
+
+        if (numerolf >= 0 ) {
+            System.out.println("Es positivo");
+        } else {
+            System.out.println("Es negativo");
+        }
+
+    }
+
+}
